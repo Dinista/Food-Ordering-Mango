5686cbeb80a22bc361bec096b1aed1dc2b190b1c
dinista <ra110752@uem.br>
2024-06-25 17:18:37 -0300

    footer

--- front-end/src/Footer.jsx
+++ front-end/src/Footer.jsx
@@ -4,6 +4,9 @@ import logo1 from './assets/unnamed(1).png'
 import logo2 from './assets/unnamed(2).png'
 import logo3 from './assets/unnamed(3).png'
 import logo4 from './assets/unnamed(4).png'
+import logo5 from './assets/unnamed(5).png'
+import logo6 from './assets/unnamed(6).png'
+import logoMango from './assets/logo.png'
 
 const Footer = () => {
     return (
@@ -15,7 +18,15 @@ const Footer = () => {
             </div>
             <div>
                 <img src={logo4} className='logos-footer4'></img>
-                <img src={logo5} className='logos-footer4'></img>
+                <img src={logo5} className='logos-footer5'></img>
+                <img src={logo6} className='logos-footer6'></img>
+            </div>
+            <div className='footer-content'>
+                <div className='footer-content-container'>
+                    <img src={logoMango} className='footer-logo' ></img>
+                    <h1>Enjoy 1000+ foods from 400 restaurants</h1>
+                    <button>Order Now</button>
+                </div>
             </div>
         </section>
     );
